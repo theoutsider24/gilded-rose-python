e7baaf7599235792a74e27bfcdf182251ecf2d41
theoutsider24 <mattcoyle4@gmail.com>
2026-02-27 08:25:58 +0000

    Add enum for items, add sell_in assertions

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,9 @@
-# -*- coding: utf-8 -*-
+from enum import StrEnum
 
+class ITEMTYPE(StrEnum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 class GildedRose:
     def __init__(self, items):
@@ -8,29 +12,28 @@ class GildedRose:
     def update_quality(self):
         for item in self.items:
             if (
-                item.name != "Aged Brie"
-                and item.name != "Backstage passes to a TAFKAL80ETC concert"
+                item.name not in [ITEMTYPE.AGED_BRIE, ITEMTYPE.BACKSTAGE_PASS]
             ):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != ITEMTYPE.SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == ITEMTYPE.BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != ITEMTYPE.SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != ITEMTYPE.AGED_BRIE:
+                    if item.name != ITEMTYPE.BACKSTAGE_PASS:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != ITEMTYPE.SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
